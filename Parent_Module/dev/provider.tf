27897e1b318d6ad49f48c78e6b0bba16e8fa8ce0
gunjan <info2rajsingh@gmail..com>
2025-12-21 16:16:13 +0530

    version added

--- Parent_Module/dev/provider.tf
+++ Parent_Module/dev/provider.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_version = ">= 1.5.0"
+provider "azurerm" {
+  features {}
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.52.0"
-    }
-  }
-
-  backend "azurerm" {}
+  subscription_id = "5235c2c1-03cf-4091-87b8-e8adb1a1c5f6"
 }
-
--- Parent_Module/dev/version.tf
+++ Parent_Module/dev/version.tf
@@ -1,5 +1,13 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_version = ">= 1.5.0"
 
-  subscription_id = "5235c2c1-03cf-4091-87b8-e8adb1a1c5f6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.52.0"
+    }
+  }
+
+  backend "azurerm" {}
 }
+
