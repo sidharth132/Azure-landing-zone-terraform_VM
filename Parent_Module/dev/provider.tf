0c97c1d2da334d228cea47026c6712ef37261cb1
gunjan <info2rajsingh@gmail..com>
2025-12-21 16:06:55 +0530

    version added

--- Parent_Module/dev/provider.tf
+++ Parent_Module/dev/provider.tf
@@ -11,8 +11,3 @@ terraform {
   backend "azurerm" {}
 }
 
-provider "azurerm" {
-  features {}
-
-  subscription_id = "5235c2c1-03cf-4091-87b8-e8adb1a1c5f6"
-}
---
+++ Parent_Module/dev/version.tf
@@ -0,0 +1,5 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "5235c2c1-03cf-4091-87b8-e8adb1a1c5f6"
+}
